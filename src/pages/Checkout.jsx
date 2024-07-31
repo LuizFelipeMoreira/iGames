5607fedba6f1accc3a697330dd66e87e30223d0d
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-07-31 01:11:11 -0300

    estrutura inicial pagina checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useProduct } from '../context/ProductContext';
+import {
+  Container,
+  Item,
+  SelectedItems,
+  ShoopingArea,
+} from '../styles/pages/Checkout';
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const { productsBag } = useProduct();
+
+  return (
+    <Container>
+      <SelectedItems>
+        <h1>Checkout</h1>
+        {productsBag.map((item) => (
+          <Item key={item.id}>
+            <img src={item.image} alt={item.title} />
+
+            <div>
+              <p>{item.title}</p>
+              <p>{item.price}</p>
+            </div>
+          </Item>
+        ))}
+      </SelectedItems>
+      <ShoopingArea></ShoopingArea>
+    </Container>
+  );
 };
 
 export default Checkout;
---
+++ src/styles/pages/Checkout.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import Theme from '../../theme/Theme';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: 100%;
+  padding: 2rem 3rem;
+  margin: 0 auto;
+  font-family: 'Poppins';
+
+  @media ${Theme.device.tablet} {
+    padding: 10px;
+  }
+
+  @media ${Theme.device.mobile} {
+    padding: 0 1rem;
+  }
+`;
+
+export const SelectedItems = styled.section`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  max-height: 550px;
+  gap: 10px;
+  overflow-y: auto;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  // background: burlywood;
+  border-bottom: 1px solid gray;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    width: 100%;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    max-height: 60px;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    border-radius: 5px;
+  }
+`;
+
+export const TotalPrice = styled.section``;
+
+export const ShoopingArea = styled.section``;
+
+export const Payment = styled.div``;
